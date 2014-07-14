7054633a4a2fa3a72b23c578aa6ec919a7f3e543
bluezebrazhang <haominzhang@Haomins-MacBook-Air.local>
2014-07-13 20:16:05 -0400

    my answer

--- LongestConsecutiveSequence.java
+++ LongestConsecutiveSequence.java
@@ -21,3 +21,35 @@
         }
         return Math.max(curLen,maxLen);
     }
+
+    //better solution
+    //http://www.programcreek.com/2013/01/leetcode-longest-consecutive-sequence-java/
+    public int longestConsecutive(int[] num) {
+    Set<Integer> set = new HashSet<Integer>();
+    int max = 1;
+ 
+    for (int e : num)
+        set.add(e);
+ 
+    for (int e : num) {
+        int left = e - 1;
+        int right = e + 1;
+        int count = 1;
+ 
+        while (set.contains(left)) {
+            count++;
+            set.remove(left);
+            left--;
+        }
+ 
+        while (set.contains(right)) {
+            count++;
+            set.remove(right);
+            right++;
+        }
+ 
+        max = Math.max(count, max);
+    }
+ 
+    return max;
+}
